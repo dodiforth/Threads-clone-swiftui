50531e8004bb90438921fd3e9ee2778d250b5da8
Dowon Kim <dowoniscool@yahoo.com>
2024-03-22 19:32:22 +0100

    Initial commit - project folder setup

---
+++ ThreadsClone/App/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Dowon Kim on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Dowon Kim on 22/03/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
