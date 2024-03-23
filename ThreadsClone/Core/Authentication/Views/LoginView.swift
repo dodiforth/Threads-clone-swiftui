e4e6f26a861dc486dad1846defa2358219dda90c
Dowon Kim <dowoniscool@yahoo.com>
2024-03-23 15:24:56 +0100

    Added asset images and LoginView UI

--- ThreadsClone/Core/Authentication/Views/LoginView.swift
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,83 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    let iconWidth = 120.0
+    let iconHeight = 120.0
+    
+    let loginButtonWidth = 350.0
+    let loginButtonHeight = 44.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: iconWidth, height: iconHeight)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                NavigationLink{
+                    Text("Forgot Password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .padding(.vertical)
+                        .padding(.trailing, 20)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    print("Login button tapped")
+                } label: {
+                    Text("Login")
+                        .fontWeight(.semibold)
+                        .frame(width: loginButtonWidth,height: loginButtonHeight)
+                        .background(.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don\'t have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(.black)
+                }
+                .padding(.vertical, 16)
+            }
+            
+        }
     }
 }
 
