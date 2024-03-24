1ca2d18c16083bb8019aa752e4da77e7163c3bb9
Dowon Kim <dowoniscool@yahoo.com>
2024-03-24 10:04:55 +0100

    Style : wrap up FeedView for now

--- ThreadsClone/Core/Feed/View/FeedView.swift
+++ ThreadsClone/Core/Feed/View/FeedView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("hello feed!!!!!")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id:\.self){ thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG : Refresh Threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    FeedView()
+    NavigationStack {
+        FeedView()
+    }
 }
