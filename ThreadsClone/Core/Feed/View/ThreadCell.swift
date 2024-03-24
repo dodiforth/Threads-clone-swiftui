3ee23a66be1b2c81281153e97c0d2515733e4c09
Dowon Kim <dowoniscool@yahoo.com>
2024-03-24 09:36:31 +0100

    Style : Added ThreadCell

---
+++ ThreadsClone/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,77 @@
+//
+//  ThreadCell.swift
+//  ThreadsClone
+//
+//  Created by Dowon Kim on 23/03/2024.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12){
+                Image("cat-mimi2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4){
+                    
+                    HStack(){
+                        Text("dodinoyo")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("Award Winning Cat Owner")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16){
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            Divider()
+        }
+        .padding()
+
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
