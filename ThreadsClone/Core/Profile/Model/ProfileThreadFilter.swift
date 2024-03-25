7ecdcf918648124dc27d41b2fd5919450ef7edc9
Dowon Kim <dowoniscool@yahoo.com>
2024-03-25 09:09:19 +0100

    Add ProfileThreadFilter view to display thread filters

---
+++ ThreadsClone/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileThreadFilter.swift
+//  ThreadsClone
+//
+//  Created by Dowon Kim on 25/03/2024.
+//
+
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+    case threads
+    case replies
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
--- ThreadsClone/Core/Profile/View/ProfileView.swift
+++ ThreadsClone/Core/Profile/View/ProfileView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var filterBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 20
+    }
+    
     var body: some View {
-        Text("I am who i am")
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    // bio and stats
+                    VStack(alignment: .leading, spacing: 12) {
+                        // fullname and username
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Mimi Leclerc")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            Text("mimi_leclerc")
+                                .font(.subheadline)
+                        }
+                        
+                        Text("Living in Brest and looking out windows")
+                            .font(.footnote)
+                        Text("2 followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    Spacer()
+                    
+                    CircularProfileImageView()
+                }
+                
+                Button{
+                    
+                } label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 362, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                // user content list view
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases){ filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : . regular)
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .foregroundColor(.black)
+                                        .matchedGeometryEffect(id: "item", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .foregroundColor(.clear)
+                                }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring()){
+                                    selectedFilter = filter
+                                }
+                            }
+                        }
+                    }
+                    LazyVStack {
+                        ForEach(0 ... 10, id:\.self){ thread in
+                            ThreadCell()
+                        }
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
