9c0b9f588c237bc21b34741cec311af490128a8f
Dowon Kim <dowoniscool@yahoo.com>
2024-03-26 18:55:22 +0100

    Style : Added Create Thread View

--- ThreadsClone/Core/ThreadCreation/View/CreateThreadView.swift
+++ ThreadsClone/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    
+    @State private var caption = ""
+    
     var body: some View {
-        Text("Let's hear from you !")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top){
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("mimicharleskim")
+                            .fontWeight(.semibold)
+                        TextField("Start a thread...", text: $caption, axis: .vertical)
+                            .font(.footnote)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
